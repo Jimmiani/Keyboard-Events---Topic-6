477fb3a8e032b481680500eb9d0327998528faaf
Emmett Cornies <339761330@GoTVDSB.ca>
2024-12-11 09:40:01 -0500

    not done

--- Keyboard Events - Topic 6/Game1.cs
+++ Keyboard Events - Topic 6/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
 
 namespace Keyboard_Events___Topic_6
 {
@@ -11,8 +12,10 @@ namespace Keyboard_Events___Topic_6
         private SpriteBatch _spriteBatch;
 
         Random generator;
-        Texture2D pacRTexture, pacLTexture, pacUTexture, pacDTexture, pacSTexture, currentPacTexture;
-        Rectangle window, pacLocation;
+        Texture2D pacRTexture, pacLTexture, pacUTexture, pacDTexture, pacSTexture, currentPacTexture, exitTexture, barrierTexture, coinTexture;
+        Rectangle window, pacLocation, barrierRect1, barrierRect2, exitRect;
+        List<Rectangle> coins;
+        List<Rectangle> barriers;
         Vector2 pacSpeed;
         KeyboardState keyboardState, prevKeyboardState;
         MouseState mouseState;
@@ -28,31 +31,48 @@ namespace Keyboard_Events___Topic_6
 
         protected override void Initialize()
         {
-            window = new Rectangle(0, 0, 800, 600);
+            window = new Rectangle(0, 0, 800, 480);
             _graphics.PreferredBackBufferWidth = window.Width;
             _graphics.PreferredBackBufferHeight = window.Height;
             _graphics.ApplyChanges();
+            coins = new List<Rectangle>();
+            barriers = new List<Rectangle>();
+            
+            base.Initialize();
 
-            pacSize = 75;
+
+            pacSize = 60;
             pacLocation = new Rectangle(10, 10, pacSize, pacSize);
             currentPacTexture = pacSTexture;
             spedUp = false;
             slowedDown = true;
             isBig = false;
+            barriers.Add(new Rectangle(0, 250, 350, 75));
+            barriers.Add(new Rectangle(450, 250, 350, 75));
+            
+            coins.Add(new Rectangle(250, 10, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(475, 50, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(200, 400, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(400, 400, coinTexture.Width, coinTexture.Height));
+            exitRect = new Rectangle(700, 380, 100, 100);
             generator = new Random();
-
-            base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            // Pac-Man
             pacRTexture = Content.Load<Texture2D>("PacRight");
             pacLTexture = Content.Load<Texture2D>("PacLeft");
             pacUTexture = Content.Load<Texture2D>("PacUp");
             pacDTexture = Content.Load<Texture2D>("PacDown");
             pacSTexture = Content.Load<Texture2D>("PacSleep");
+
+            // Others
+            exitTexture = Content.Load<Texture2D>("hobbit_door");
+            coinTexture = Content.Load<Texture2D>("coin");
+            barrierTexture = Content.Load<Texture2D>("rock_barrier");
         }
 
         protected override void Update(GameTime gameTime)
@@ -64,7 +84,7 @@ namespace Keyboard_Events___Topic_6
             prevKeyboardState = keyboardState;
             keyboardState = Keyboard.GetState();
             pacSpeed = new Vector2();
-
+           
 
             // Speed and Textures
             if (pacSpeed.X == 0 && pacSpeed.Y == 0)
@@ -118,7 +138,7 @@ namespace Keyboard_Events___Topic_6
             }
             if (spedUp)
             {
-                pacSpeed *= 7;
+                pacSpeed *= 8;
             }
 
             // Slow down
@@ -140,7 +160,6 @@ namespace Keyboard_Events___Topic_6
                 pacLocation.Height *= 3;
                 pacSize *= 3;
                 isBig = true;
-               
             }
             
 
@@ -152,6 +171,29 @@ namespace Keyboard_Events___Topic_6
 
             pacLocation.Offset(pacSpeed);
 
+
+            // Topic 8
+
+            for (int i = 0; i < coins.Count; i++)
+            {
+                if (pacLocation.Intersects(coins[i]))
+                {
+                    coins.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            if (exitRect.Contains(pacLocation))
+            {
+                Exit();
+            }
+
+            foreach (Rectangle barrier in barriers)
+            {
+                if (pacLocation.Intersects(barrier))
+                    pacLocation.Offset(-pacSpeed);
+            }
+
             base.Update(gameTime);
         }
 
@@ -160,8 +202,13 @@ namespace Keyboard_Events___Topic_6
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             _spriteBatch.Begin();
-            
+
+            _spriteBatch.Draw(barrierTexture, barrierRect1, Color.White);
+            _spriteBatch.Draw(barrierTexture, barrierRect2, Color.White);
+            _spriteBatch.Draw(exitTexture, exitRect, Color.White);
             _spriteBatch.Draw(currentPacTexture, pacLocation, Color.White);
+            foreach (Rectangle coin in coins)
+                _spriteBatch.Draw(coinTexture, coin, Color.White);
             
             _spriteBatch.End();
 
